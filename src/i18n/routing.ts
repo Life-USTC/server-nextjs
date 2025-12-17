1c29d2a558903e0dc22b70d3cdf62474c66a7b91
TianKai Ma <tiankaima@163.com>
2025-12-17 17:58:53 +0800

    feat: add i18n routing and middleware configuration

---
+++ src/i18n/routing.ts
@@ -0,0 +1,18 @@
+import { createNavigation } from "next-intl/navigation";
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  // A list of all locales that are supported
+  locales: ["en-us", "zh-cn"],
+
+  // Used when no locale matches
+  defaultLocale: "en-us",
+
+  // Locale detection strategy
+  localeDetection: true,
+});
+
+// Lightweight wrappers around Next.js' navigation APIs
+// that will consider the routing configuration
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing);
